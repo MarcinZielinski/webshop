f4e260b4a310f8a9e80c00fff94d3c1fe66ec68c
Mrz355 <zielinskimarcin123@gmail.com>
2017-12-27 22:51:50 +0100

    create basic web-app with spark framework to make orders on products

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
+import entities.Product;
+import entities.TransactionEntity;
 import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
+import static spark.Spark.*;
 /**
  * Created by Marcin on 2017-12-12.
  */
@@ -34,9 +41,46 @@ public class Main {
     public static void main(final String[] args) throws Exception {
         try(final Session session = getSession()) {
             Transaction tx = session.beginTransaction();
-            Generator.inheritanceTest(session);
+            Generator.populateDatabase(session);
             tx.commit();
         }
+
+        get("/hello", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new VelocityTemplateEngine().render(
+                    new ModelAndView(model, "/buying-layout.vm")
+            );
+        });
+
+        post("/buy", (request, response) -> {
+            String a, b;
+            a = request.queryParams("quantity");
+            b = request.queryParams("productId");
+            Map<String, Object> model = new HashMap<>();
+
+            if(buyProduct(Long.parseLong(a),Long.parseLong(b)))
+                return new VelocityTemplateEngine().render(
+                        new ModelAndView(model, "/bought-layout.vm"));
+            else
+                return new VelocityTemplateEngine().render(
+                        new ModelAndView(model, "/failed-to-bought-layout.vm"));
+        });
+    }
+
+    private static Boolean buyProduct(Long quantity, Long productId) {
+        Boolean res;
+        try(final Session session = getSession()) {
+            Transaction tx = session.beginTransaction();
+            Product product = session.get(Product.class, productId);
+            res = product != null;
+            if(res) {
+                TransactionEntity transactionEntity = new TransactionEntity(quantity);
+                transactionEntity.addProduct(product);
+                session.persist(transactionEntity);
+            }
+            tx.commit();
+        }
+        return res;
     }
 
     public static void JPAmain(final String[] args) throws Exception {
